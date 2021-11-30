0b9e7440cbda5b71166be4b240cfe278e5f0e986
18895398066 <kaikai888>
2021-11-30 16:16:08 +0800

    重点人员查询接口参数化

--- testcase/test_query_api.py
+++ testcase/test_query_api.py
@@ -4,56 +4,77 @@
 # @File: test_query_api.py
 # @Software :PyCharm
 import requests
-import unittest
 from api.api_query import Api_Query
 from base.base import Run_Main
+import unittest, time
 import warnings
+from parameterized import parameterized
+from config.config import get_data, write_excel, Clear_excel
+from util import BASE_DIR, BASE_URL, get_token
 from config.config import Logger
-from util import get_token
 
 mylogger = Logger(logger="test_query_api").getlog()
 
+actual_code_list = []
+actual_status_list = []
+result_list = []
+path = BASE_DIR + "/database/queryBlacklist.xlsx"
 
-class Test_Query_Api(unittest.TestCase):
+
+class Test_Api_Query(unittest.TestCase):
     @classmethod
-    def setUpClass(cls):
-        mylogger.info("开始测试查询接口")
+    def setUpClass(cls) -> None:
+        mylogger.info("开始测试Test_Api_Query")
+        cls.query_api = Api_Query()
+
+        # cls.run = Run_Main()
 
     @classmethod
-    def tearDownClass(cls):
-        mylogger.info("结束测试查询接口")
+    def tearDownClass(cls) -> None:
+        mylogger.info("结束测试Test_Api_Query")
 
-    def setUp(self):
-        mylogger.info("-------开始执行测试用例-------")
-        warnings.simplefilter('ignore', ResourceWarning)  # 消除报警的
-        self.api_query = Api_Query()
+    def setUp(self) -> None:
         self.run = Run_Main()
+        mylogger.info("开始执行测试用例")
+        warnings.simplefilter('ignore', ResourceWarning)
 
-    def tearDown(self):
-        mylogger.info("-----结束一条测试用例------")
+    def tearDown(self) -> None:
         self.run.close_session()
+        mylogger.info("执行完成1条测试用例")
 
-    def test_query_api_success(self):
-        url = "https://smart-uat.gtdreamlife.com:18762/api/ioc/blacklist/queryBlacklist"
-        json = {
-            "startTime": "2021-09-01",
-            "endTime": "2021-11-16",
-            "projectIds": "t1",
-            "pageNum": 1
-        }
+    @parameterized.expand(x[:-3] for x in get_data(path))
+    def test_fire_and_security_api(self, case_name, api, request_body, headers, method, expect_code, status_code):
+        """
+
+        :param case_name:
+        :param api:
+        :param headers:
+        :param method:
+        :param expect_code:
+        :param status_code:
+        :return:
+        """
+        mylogger.info("测试用例名称：{}".format(case_name))
+        url = BASE_URL + api
+        mylogger.info("url为：{}".format(url))
         token = get_token()
-        print(token)
-        headers = {"Content-Type": "application/json",
-                   "Authorization": token}
-        method = "post"
-        response = self.api_query.test_query_api(url, json, headers, method)
+        headers["Authorization"] = token
+        response = self.query_api.test_query_api(url=url, json=request_body, headers=headers,
+                                                 method=method)
         jsondata = response.json()
+        actual_code = jsondata.get("errorCode")
+        actual_status = response.status_code
         mylogger.info(jsondata)
-        if jsondata.get("errorCode") == 0:
-            mylogger.info("errorCode为0，测试通过")
-        if response.status_code == 200:
-            mylogger.info("status_code为200，测试通过")
+        if expect_code == jsondata.get("errorCode"):
+            mylogger.info("errorCode为{},测试通过".format(expect_code))
+        if status_code == response.status_code:
+            mylogger.debug("status_code为{},测试通过".format(status_code))
+            result = "Pass"
         else:
             mylogger.info("测试不通过！")
-        self.assertEqual(0, jsondata.get("errorCode"))
-        self.assertEqual(200, response.status_code)
+            result = "Fail"
+        self.assertEqual(expect_code, jsondata.get("errorCode"))
+        self.assertEqual(status_code, response.status_code)
+        actual_code_list.append(actual_code)
+        actual_status_list.append(actual_status)
+        result_list.append(result)
